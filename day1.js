7c3ec3d89f9e1b0adf6b537013b2dcf43bb3ef29
简素言也 <409914837@qq.com>
2021-10-20 18:03:18 +0800

    day1

---
+++ day1.js
@@ -0,0 +1,65 @@
+// debounce 防抖,多次只执行最后一次
+function debounce(fn, time) {
+  let b = false;
+  let a;
+  if (!b) {
+    b = true;
+    a = setTimeout(() => {
+      fn();
+      b = false;
+    }, time);
+  } else {
+    clearTimeout(a);
+    a = setTimeout(() => {
+      fn();
+      b = false;
+    }, time);
+  }
+}
+
+// trottle 节流,每隔一段时间执行一次
+function trottle(fn, time) {
+  let a = false;
+  if (!a) {
+    a = true;
+    fn();
+    setTimeout(() => {
+      a = false;
+    }, time);
+  }
+}
+
+// promise.all  输入是由多个Promise对象组成的一个数组，最后输出一个新的Promise对象。成功时返回的是一个结果数组，和输入的promise数组顺序是一致的，失败时则返回最先被reject的错误值。   并展示一个例子
+
+function promiseAll(promises) {
+  return new Promise((resolve, reject) => {
+    let results = new Array(promises.length);
+    let count = 0;
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(
+        (res) => {
+          results[i] = res;
+          count += 1;
+          if (count === promises.length) {
+            return resolve(results);
+          }
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+    }
+  });
+}
+
+let p1 = new Promise((resolve) => resolve("p1"));
+let p2 = new Promise((resolve) => resolve("p2"));
+let p3 = Promise.reject("p3 error");
+
+promiseAll([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
